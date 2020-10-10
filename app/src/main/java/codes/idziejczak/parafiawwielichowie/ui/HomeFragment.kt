713061d27b6d37595dc7b7e529ec07f33f4ee3fd
Luncenok <idziejczak3@gmail.com>
2020-10-10 19:45:18 +0200

    Create and setup connections between home- and ogloszeniaFragment

--- app/src/main/java/codes/idziejczak/parafiawwielichowie/ui/HomeFragment.kt
+++ app/src/main/java/codes/idziejczak/parafiawwielichowie/ui/HomeFragment.kt
@@ -6,10 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import codes.idziejczak.parafiawwielichowie.R
 import codes.idziejczak.parafiawwielichowie.databinding.FragmentHomeBinding
+import codes.idziejczak.parafiawwielichowie.viewmodels.HomeViewModel
 
 class HomeFragment : Fragment() {
+
+    private val viewModel: HomeViewModel by lazy {
+        ViewModelProvider(this, HomeViewModel.Factory()).get(HomeViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,8 +25,17 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentHomeBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        activity?.actionBar?.title = getString(R.string.app_name)
 
-
+        viewModel.navigateToOgloszenia.observe(viewLifecycleOwner, {
+            if (it == true) {
+                this.findNavController()
+                    .navigate(HomeFragmentDirections.actionHomeFragmentToOgloszeniaFragment())
+                viewModel.doneNavigating()
+            }
+        })
 
         return binding.root
     }
---
+++ app/src/main/java/codes/idziejczak/parafiawwielichowie/viewmodels/HomeViewModel.kt
@@ -0,0 +1,32 @@
+package codes.idziejczak.parafiawwielichowie.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class HomeViewModel : ViewModel() {
+
+    private val _navigateToOgloszenia = MutableLiveData<Boolean?>()
+    val navigateToOgloszenia: LiveData<Boolean?>
+        get() = _navigateToOgloszenia
+
+    fun beginNavigate() {
+        _navigateToOgloszenia.value = true
+    }
+
+    fun doneNavigating() {
+        _navigateToOgloszenia.value = null
+    }
+
+    class Factory : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            @Suppress("UNCHECKED_CAST")
+            if (modelClass.isAssignableFrom(HomeViewModel::class.java))
+                return HomeViewModel() as T
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+
+    }
+
+}
